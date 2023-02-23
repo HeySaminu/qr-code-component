da5c9f569ba471c5bc553159136c2e75536b2104
Saminu Eedris <hey@saminu.me>
2023-02-23 18:20:27 +0100

    Adding <main> tag

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import './index.css'
 function App() {
 
   return (
-  <>
-        <div className="App">
+  <><main>
+
+<div className="App">
           <div className="QrContainer">
-            <img src="/assets/image-qr-code.png" className="QrCode" width="288" height="288"/>
+            <img src="/assets/image-qr-code.png" alt="QR Code Image" className="QrCode" width="288" height="288"/>
             <h2>Improve your front-end skills by building projects</h2>
             <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
           </div>
       </div>
+
+  </main>
   </>
   )
 }
