674ac35fde47b61ffd343e141e55aabb0a2b0190
Saminu Eedris <hey@saminu.me>
2023-02-23 17:41:12 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './index.css'
+
+function App() {
+
+  return (
+  <>
+        <div className="App">
+          <div className="QrContainer">
+            <img src="/src/assets/image-qr-code.png" className="QrCode" width="288" height="288"/>
+            <h2>Improve your front-end skills by building projects</h2>
+            <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+          </div>
+      </div>
+  </>
+  )
+}
+
+export default App
