e693c8a4f4857ad02c46cff6c0a56a57db3c7f33
Saminu Eedris <hey@saminu.me>
2023-02-23 18:09:25 +0100

    Final fixes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   <>
         <div className="App">
           <div className="QrContainer">
-            <img src="/src/assets/image-qr-code.png" className="QrCode" width="288" height="288"/>
+            <img src="/assets/image-qr-code.png" className="QrCode" width="288" height="288"/>
             <h2>Improve your front-end skills by building projects</h2>
             <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
           </div>
