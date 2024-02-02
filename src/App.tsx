e6bb023fb09cbc418f7213f8716cd054cdd209a5
Saminu Eedris <hey@saminu.me>
2024-02-02 16:23:11 +0100

    Adding a level-one heading

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
 <div className="App">
           <div className="QrContainer">
             <img src="/assets/image-qr-code.png" alt="QR Code Image" className="QrCode" width="288" height="288"/>
-            <h2>Improve your front-end skills by building projects</h2>
+            <h1>Improve your front-end skills by building projects</h1>
             <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
           </div>
       </div>
